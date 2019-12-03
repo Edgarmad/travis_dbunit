147cf88838a4864b34ddea813ef6ce065728c087
edgarmad <edmad1396@gmail.com>
2019-12-03 10:46:12 -0600

    testeo 15

--- src/test/java/com/mayab/calidad/travis/AlumnoDbTest.java
+++ src/test/java/com/mayab/calidad/travis/AlumnoDbTest.java
@@ -98,12 +98,9 @@ public class AlumnoDbTest extends DBTestCase {
     @Test
     public void testGet()throws Exception{
         IDatabaseConnection conn = getConnection();
-        Alumno a = new Alumno("15","Carla","Pera",20,79.7f);
-        AlumnoDAO dao= new Alumnos();
-        dao.getAlumno("15");
         IDataSet databaseDataSet = getConnection().createDataSet();
         ITable actualTable = databaseDataSet.getTable("alumno");
-        InputStream xmlFile = getClass().getResourceAsStream("/get.xml");
+        InputStream xmlFile = getClass().getResourceAsStream("/data.xml");
 	IDataSet expectedDataSet = new FlatXmlDataSetBuilder().build(xmlFile);
 	ITable expectedTable = expectedDataSet.getTable("alumno");
         Assertion.assertEquals(expectedTable, actualTable);
